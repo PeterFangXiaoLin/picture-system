781ef27ed7c9482963344cc51868308d33913497
PeterFangXiaoLin <1565906107@qq.com>
2024-12-24 23:16:02 +0800

    前端项目初始化

---
+++ web/picture-frontend/src/main.ts
@@ -0,0 +1,20 @@
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import 'virtual:uno.css'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
